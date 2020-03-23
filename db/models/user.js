a6cdfceccc4fbfbdbe9c48042c56315bea5f62e0
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-03-23 10:27:17 -0400

    added hooks and associations to db/models/user

--- db/models/user.js
+++ db/models/user.js
@@ -1,4 +1,7 @@
 'use strict';
+const { hashPassword } = require("../../helpers/encryption/encrypt.encryption");
+
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     email: DataTypes.STRING,
@@ -6,9 +9,18 @@ module.exports = (sequelize, DataTypes) => {
     username: DataTypes.STRING,
     password: DataTypes.TEXT,
     profile_pic: DataTypes.TEXT
-  }, {});
+  }, {
+    hooks: {
+      beforeCreate: async (user) => {
+        user.password = await hashPassword(user.password)
+      }
+    }
+  });
   User.associate = function(models) {
     // associations can be defined here
+    User.hasMany(models.Post, { foreignKey: 'user_id'});
+    User.hasMany(models.Follower, { foreignKey: 'user_id'})
+    User.hasMany(models.Follower, { foreignKey: 'follower_id'})
   };
   return User;
 };
