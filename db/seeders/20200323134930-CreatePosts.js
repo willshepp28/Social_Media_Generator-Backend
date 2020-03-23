3a7fa35758cecf61ffbf415d859ec99d4911185a
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-03-23 10:16:04 -0400

    returning users to sequelize queryInterface method to seed db with posts in db/seeders/createPosts.js

--- db/seeders/20200323134930-CreatePosts.js
+++ db/seeders/20200323134930-CreatePosts.js
@@ -12,9 +12,15 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
+   return User.count().then(totalUsers => {
+    return loadPictures(totalUsers).then(posts => {
+      console.log(posts);
+      return queryInterface.bulkInsert("Posts", posts,{})
+    })
+  })
   },
-
   down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Posts", null, {});
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
