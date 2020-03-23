e10888a6bd16721cb9db64092b000cfb70f118d1
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-03-23 10:14:36 -0400

    included queryInterface.bulkDelete() in db/seeder/createUser

--- db/seeders/20200323134922-CreateUsers.js
+++ db/seeders/20200323134922-CreateUsers.js
@@ -28,6 +28,7 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Users", null, {});
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
